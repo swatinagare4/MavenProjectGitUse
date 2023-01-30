7fe64c11ab4f5dff2ea5a3c318c5fe92fee904dd
swatinagare4 <n.swati111@gmail.com>
2023-01-30 19:02:19 +0530

    changes by swati nagare

---
+++ src/main/java/com/Swati/GitUseMavenProject1Application.java
@@ -0,0 +1,13 @@
+package com.Swati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitUseMavenProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitUseMavenProject1Application.class, args);
+	}
+
+}
